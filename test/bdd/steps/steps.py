29883dc47afb67312d88d8019ea35b37fd2efed0
JennieRamos <jennieramos06@gmail.com>
2014-12-15 20:58:24 +0800

    modified

--- test/bdd/steps/steps.py
+++ test/bdd/steps/steps.py
@@ -1,12 +1,4 @@
 from lettuce import *
 from score.score import *
 
-@step(u'Given that I have two teams')
-def given_that_i_have_two_teams(step):
-    assert True
-@step(u'When the user will input a score on a team')
-def when_the_user_will_input_a_score_on_a_team(step):
-    assert True
-@step(u'Then it will record, total and display the score')
-def then_it_will_record_total_and_display_the_score(step):
-    assert True
+
